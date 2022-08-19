882e8f88a7563a9a02e45bc212bb5d9ba90b8e83
shady00s <ssk2310@hotmail.com>
2022-08-19 23:54:20 +0200

    update 2

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
         'OPTIONS, GET, POST, PUT, PATCH, DELETE'
     );
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials','*')
+    res.setHeader('Access-Control-Allow-Credentials',true)
     next();
 });
 
