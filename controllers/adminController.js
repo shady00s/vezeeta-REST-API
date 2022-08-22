b84958b0d592899a29c1fe68ce241393f7953f88
shady00s <ssk2310@hotmail.com>
2022-08-22 02:11:28 +0200

    update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -223,7 +223,10 @@ function deleteDrByAdmin(req, res) {
             message: "succssess",
             body: result
         })
-    })
+    }).catch(e=>  res.status(400).json({
+        message: "failed",
+        body: e
+    }))
 }
 
 
